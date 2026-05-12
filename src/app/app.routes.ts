8bef6e6ffd8fb4aba0abfeeacd308e459c1b05de
JotaPablo <jpabloazevedo987@gmail.com>
2026-05-12 02:22:20 -0300

    feat: implementa telas de Feed e Biblioteca e finaliza fluxo de navegação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Cadastro } from './cadastro/cadastro';
 import { Perfil } from './perfil/perfil';
+import { Feed } from './feed/feed';
+import { Biblioteca } from './biblioteca/biblioteca';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'cadastro', component: Cadastro },
-  { path: 'perfil', component: Perfil }
+  { path: 'perfil', component: Perfil },
+  { path: 'feed', component: Feed },
+  { path: 'biblioteca', component: Biblioteca }
 ];
