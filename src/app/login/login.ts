7d9fbb47fbd3e8b14d720a8a436907392826db82
Arthur <arthur_moreira.10@hotmail.com>
2026-05-26 10:14:32 -0300

    Formulário de login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  // Objeto para vincular aos inputs via ngModel
+  credentials = {
+    email: '',
+    password: ''
+  };
+
+  showPassword = false;
+
+  constructor(private router: Router) {}
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  // Recebe o formulário (NgForm) submetido pelo HTML
+  onSubmit(loginForm: NgForm) {
+    if (loginForm.valid) {
+      console.log('Dados do login submetidos:', this.credentials);
+      
+      // Lógica de autenticação entraria aqui
+      
+      this.router.navigate(['/feed']);
+    } else {
+      // Força a exibição dos erros se o utilizador tentar submeter um formulário inválido
+      Object.keys(loginForm.controls).forEach(field => {
+        const control = loginForm.controls[field];
+        control.markAsTouched({ onlySelf: true });
+      });
+    }
+  }
+}
